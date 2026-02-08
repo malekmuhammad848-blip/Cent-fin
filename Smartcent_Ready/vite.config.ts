03de475d044beb47fc686ce0088c1671f9b76797
malekmuhammad848-blip <malekmuhammad848@gmail.com>
2026-02-08 17:10:04 +0300

    Update vite.config.ts

--- Smartcent_Ready/vite.config.ts
+++ Smartcent_Ready/vite.config.ts
@@ -1,24 +1,23 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: "www", // ← مجلد الويب
-  build: {
-    outDir: "../android/app/src/main/assets/public", // ← مكان إخراج الملفات للـ Android
-    emptyOutDir: true,
-  },
+  root: 'www',          // ← هذا هو التعديل الأساسي
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: 'dist',      // هذا المجلد سينشأ بعد البناء
+    emptyOutDir: true,
   },
 });
