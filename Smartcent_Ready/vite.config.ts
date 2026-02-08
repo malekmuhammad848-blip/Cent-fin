49c419b65c154c8ab775ad2cd76af3ef7bf966d1
malekmuhammad848-blip <malekmuhammad848@gmail.com>
2026-02-08 17:25:07 +0300

    Update vite.config.ts

--- Smartcent_Ready/vite.config.ts
+++ Smartcent_Ready/vite.config.ts
@@ -8,10 +8,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  root: path.resolve(__dirname, "www"), // ← هنا حددنا مجلد www
   plugins: [react(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    outDir: path.resolve(__dirname, "dist"), // الناتج سيكون في Smartcent_Ready/dist
+    emptyOutDir: true
+  }
 });
